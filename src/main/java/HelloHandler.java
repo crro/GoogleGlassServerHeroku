7edc62bb20d7e2fc826537deceb15f9a3b308b7f
David Correa <David@Davids-MacBook-Pro-5.local>
2014-10-19 16:34:26 -0400

    stuff

--- src/main/java/HelloHandler.java
+++ src/main/java/HelloHandler.java
@@ -25,19 +25,23 @@ public class HelloHandler extends AbstractHandler {
         if (target.equals("/incoming/")) {
             return;
         }
-        try {
-            String method = baseRequest.getMethod();
-            Thread.sleep(3000);
-            SessionHQ.getInstance().sendMessage("tkraska", "Message Start");
-            Thread.sleep(3000);
-            SessionHQ.getInstance().sendMessage("tkraska", "Message Next");
-            Thread.sleep(3000);
-            SessionHQ.getInstance().sendMessage("tkraska", "Message Previous");
-            Thread.sleep(3000);
-            SessionHQ.getInstance().sendMessage("tkraska", "Message End");
-        } catch (InterruptedException t) {
-            t.printStackTrace();
-        }
+//        try {
+//            String method = baseRequest.getMethod();
+//            Thread.sleep(3000);
+//            SessionHQ.getInstance().sendMessage("tkraska", "Message Start");
+//            Thread.sleep(3000);
+//            SessionHQ.getInstance().sendMessage("tkraska", "Message Next");
+//            Thread.sleep(3000);
+//            SessionHQ.getInstance().sendMessage("tkraska", "Message Previous");
+//            Thread.sleep(3000);
+//            SessionHQ.getInstance().sendMessage("tkraska", "Message End");
+//        } catch (InterruptedException t) {
+//            t.printStackTrace();
+//        }
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
 
     }
         //WebSocketServerConnection wbSConnection = (WebSocketServerConnection) request.getAttribute(HttpConnection.UPGRADE_CONNECTION_ATTRIBUTE);
