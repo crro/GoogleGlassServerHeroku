b0310119b55c0774403d9dbe6df12b0569a9401e
David Correa <David@Davids-MacBook-Pro-5.local>
2014-11-03 14:36:12 -0500

    Adding pieces of code to see if it works

--- src/main/java/HelloHandler.java
+++ src/main/java/HelloHandler.java
@@ -50,21 +50,21 @@ public class HelloHandler extends AbstractHandler {
                 baseRequest.setHandled(true);
                 System.out.println("The content length: " + request.getContentLength());
 
-                BufferedInputStream inputStream = new BufferedInputStream(request.getInputStream());
-                BufferedImage image = ImageIO.read(inputStream);//Now I got the image
+                //BufferedInputStream inputStream = new BufferedInputStream(request.getInputStream());
+//                BufferedImage image = ImageIO.read(inputStream);//Now I got the image
                 //I'm going to send it back just to make sure that I'm doing this properly
-                java.util.Scanner s = new java.util.Scanner(request.getInputStream()).useDelimiter("\\A");
-                //this input stream is only for the REQUEST. I need to get the content!!!
-                System.out.println("The content is:" + (s.hasNext() ? s.next() : ""));
-                OutputStream outputStream = response.getOutputStream();
+//                java.util.Scanner s = new java.util.Scanner(request.getInputStream()).useDelimiter("\\A");
+//                //this input stream is only for the REQUEST. I need to get the content!!!
+//                System.out.println("The content is:" + (s.hasNext() ? s.next() : ""));
+//                OutputStream outputStream = response.getOutputStream();
 
-                byte[] buffer = new byte[1024];
-                int len;
-                System.err.println(response);
-                while ((len =inputStream.read(buffer)) != -1) {
-                    System.err.println("wawa");
-                    outputStream.write(buffer, 0, len);
-                }
+//                byte[] buffer = new byte[1024];
+//                int len;
+//                System.err.println(response);
+//                while ((len =inputStream.read(buffer)) != -1) {
+//                    System.err.println("wawa");
+//                    outputStream.write(buffer, 0, len);
+//                }
 
 
                 //Another aproach
