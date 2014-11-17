cf09b5e0035e93ab86621a62a667138547793813
David Correa <David@Davids-MacBook-Pro-5.local>
2014-11-17 15:40:15 -0500

    Adding the jatex component

--- src/main/java/HelloHandler.java
+++ src/main/java/HelloHandler.java
@@ -83,6 +83,7 @@ public class HelloHandler extends AbstractHandler {
 //                    outputStream.write(buffer, 0, len);
 //                }
                 outputStream.write(imageBytes, 0, imageBytes.length);
+                System.out.println("WE ARE PRINTING THE FREAKING IMAGEEEE");
             } else {
                 //Since we are not requesting the notes, we just send the action to the client
                 SessionHQ.getInstance().sendAction("tkraska", action);
